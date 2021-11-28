d46b5eb5730e22cbb12ff19b2bb23bcbfba5027a
Loïc Roux <47380500+loic-roux-404@users.noreply.github.com>
2021-11-28 02:59:21 +0100

    Update personnal-osx.sh

--- local-envs/personnal-osx.sh
+++ local-envs/personnal-osx.sh
@@ -72,8 +72,8 @@ function p-office {
 function p-code {
 	brew install fish;
 	cp -f fish/config.fish ~/.config/fish/config.fish;
-	curl -sSL https://git.io/g-install | sh -s
-	curl -L https://raw.githubusercontent.com/tj/n/master/bin/n -o /usr/local/bin/n && chmod 751 /usr/local/bin/n 
+	sudo curl -sSL https://git.io/g-install | sh -s
+	sudo curl -L https://raw.githubusercontent.com/tj/n/master/bin/n -o /usr/local/bin/n && sudo chmod 751 /usr/local/bin/n 
 	brew install yarn --ignore-dependencies;
 	brew install --cask miniconda;
 	brew install php;
